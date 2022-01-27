6ddbbd9e434b2a2adc6abd221451cd3414f1e5ac
Vin E. R. Yap <vineryap@gmail.com>
2022-01-27 14:02:51 +0900

    Configured typescript.

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,25 @@
 import { defineConfig } from "vite";
 import path from "path";
+import typescript from "@rollup/plugin-typescript";
+console.log(path.resolve(__dirname, "./src"));
 
 export default defineConfig({
   build: {
     lib: {
       entry: path.resolve(__dirname, "src/index.ts"),
       name: "SubtitleMerger",
-      fileName: (format) => `subtitle-merger.${format}.ts`,
+      fileName: (format) => `${format}/index.js`,
     },
   },
+  plugins: [
+    typescript({
+      target: "es5",
+      sourceMap: true,
+      rootDir: path.resolve(__dirname, "src"),
+      declaration: true,
+      declarationDir: path.resolve(__dirname, "dist/types"),
+      exclude: path.resolve(__dirname, "node_modules/**"),
+      allowSyntheticDefaultImports: true,
+    }),
+  ],
 });
